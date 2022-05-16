f99c0554c1a99fee064f81672b1245c8bec87c27
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2022-05-16 14:34:11 +0530

    add list of beers, enhance with inventory information

--- src/main/java/guru/springframework/msscbeerservices/services/BeerService.java
+++ src/main/java/guru/springframework/msscbeerservices/services/BeerService.java
@@ -1,6 +1,9 @@
 package guru.springframework.msscbeerservices.services;
 
 import guru.springframework.msscbeerservices.web.model.BeerDto;
+import guru.springframework.msscbeerservices.web.model.BeerPagedList;
+import guru.springframework.msscbeerservices.web.model.BeerStyleEnum;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.UUID;
 
@@ -10,4 +13,6 @@ public interface BeerService {
     BeerDto saveNewBeer(BeerDto beerDto);
 
     BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+
+    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest0);
 }
--- src/main/java/guru/springframework/msscbeerservices/web/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/msscbeerservices/web/mappers/BeerMapper.java
@@ -2,9 +2,12 @@ package guru.springframework.msscbeerservices.web.mappers;
 
 import guru.springframework.msscbeerservices.domain.Beer;
 import guru.springframework.msscbeerservices.web.model.BeerDto;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 
 @Mapper(uses = {DateMapper.class})
+@DecoratedWith(BeerMapperDecorator.class)
+
 public interface BeerMapper {
     BeerDto beerToBeerDto(Beer beer);
     Beer beerDtoToBeer(BeerDto beerDto);
